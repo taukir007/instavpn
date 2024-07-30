b553b6ed472e3f231df979b7687984c0884b25c2
Taukir Ahmed <73280535+taukir007@users.noreply.github.com>
2024-07-30 15:47:17 +0600

    Update instavpn.sh

--- instavpn.sh
+++ instavpn.sh
@@ -1,14 +1,21 @@
+#!/bin/bash
+
+# Check if the instavpn directory exists and remove it if it does
 [ -d instavpn ] && rm -rf instavpn/
-echo "Installing git,cron,python3"
+
+echo "Installing git, cron, python3"
 if [[ ! -z $(which apt-get) ]]; then
-    apt-get install -y git cron python3 > /dev/null
+    sudo apt-get update > /dev/null
+    sudo apt-get install -y git cron python3 > /dev/null
     echo "Ok"
 else
     echo "You must use Ubuntu"
     exit 1
 fi
+
 echo "Cloning git repo"
 git clone https://github.com/taukir007/instavpn.git --quiet || exit 1
 echo "Ok"
+
 cd instavpn
 sudo python3 install.py
